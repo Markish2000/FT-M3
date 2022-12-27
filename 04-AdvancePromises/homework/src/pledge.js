9053dadc1a69694217f18e15018b3980cf8b74c0
Marcos Hernan Parella <marcosparella2000@gmail.com>
2022-12-26 23:10:21 -0300

    Correción cap 2

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -29,8 +29,6 @@ $Promise.prototype._internalReject = function (reason) {
   }
 };
 
-const handlers = [{}];
-
 $Promise.prototype.then = function (successCb, errorCb) {
   this._handlerGroups.push({
     successCb: typeof successCb === 'function' ? successCb : false,
