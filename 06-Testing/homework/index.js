2391a782ccfa4ddd288683b6ed39a439a8f4a962
Marcos Hernan Parella <marcosparella2000@gmail.com>
2022-12-30 23:22:01 -0300

    j

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -15,4 +15,4 @@ app.post('/product', (req, res) => {
   });
 });
 
-module.exports = app; // Exportamos app para que supertest session la pueda
+module.exports = app; // Exportamos app para que supertest session la pued
